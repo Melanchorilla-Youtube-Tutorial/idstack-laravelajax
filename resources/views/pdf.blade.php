ae39abe6a82d0db1ec99dba97b926100c8a7ff24
melanchorilla <dickykamaludin12@gmail.com>
2021-10-04 19:48:49 +0700

    first commit

---
+++ resources/views/pdf.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Export PDF</title>
+</head>
+<body>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nama</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($contacts as $contact)
+            <tr>
+                <td>{{ $contact->id }}</td>
+                <td>{{ $contact->name }}</td>
+                <td>{{ $contact->email }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+</html>
